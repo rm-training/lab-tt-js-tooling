38495e89e74fededfc556914b21fa141ec90ddb9
Ryan Morris <mr.morris@gmail.com>
2022-04-12 18:55:45 -0400

    Prettier formatting

--- src/1-lint-me.js
+++ src/1-lint-me.js
@@ -2,6 +2,11 @@ x = 10;
 y = 100;
 var y;
 
+
+const myValues = [1, 2, 3, 4, 5];
+
+const myUser = {id: 5, name: "Mandy", jump:function(){console.log("jumping")}}
+
 function rad(z) {
   x = z;
   console.log(x, z);
